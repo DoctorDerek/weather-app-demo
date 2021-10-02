985aa411ab8e9c410632aef93fa8f88bcaa677d2
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-02 10:55:34 -0700

    chore: create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // These settings are handled in .editorconfig:
+  tabWidth: 2, // indent_size = 2
+  useTabs: false, // indent_style = space
+  endOfLine: "lf", // end_of_line = lf
+  semi: false, // default: true
+  singleQuote: false,
+  printWidth: 80,
+  trailingComma: "es5",
+  bracketSpacing: true,
+}
