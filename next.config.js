6e5500eaf9cb3ba39fbdcc0e08eefb839db9506e
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-02 10:54:34 -0700

    chore: create next.config.js using esbuild-loader

---
+++ next.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+
+  webpack5: true,
+
+  webpack: (config, { dev, isServer, webpack }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]",
+          },
+        },
+      ],
+    })
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    })
+
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      loader: "esbuild-loader",
+      options: { loader: "tsx", target: "esnext" },
+    })
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: "react",
+      })
+    ) // Support JSX Transform per https://dev.to/rsa/speed-up-next-js-build-with-typescript-and-tailwind-css-418d
+
+    return config
+  },
+}
