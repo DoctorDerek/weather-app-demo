bc4722e00b03da725c5b9ad39716c5a861f98c19
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-03 14:03:01 -0700

    fix: reduce "w-64" to "w-48" in <Card> to match design document

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ export default function Card({
   heading: string
 }) {
   return (
-    <div className="flex flex-col items-center justify-center w-64 p-4 mx-auto text-gray-400 bg-white rounded-lg shadow-md drop-shadow-md">
+    <div className="flex flex-col items-center justify-center w-48 p-4 mx-auto text-gray-400 bg-white rounded-lg shadow-md drop-shadow-md">
       <h2 className="font-bold text-gray-600 uppercase">{heading}</h2>
       {children && children}
     </div>
