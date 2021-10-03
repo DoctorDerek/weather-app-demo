72a6e58654f725d1255b1b1c1ec9d90e9df87518
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-02 18:16:09 -0700

    refactor: separate <App> component from @/src/pages/index.tsx page logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,5 @@
-import { useState } from "react"
-
-import { CityWeather } from "@/src/components/CityWeather"
+import App from "@/src/components/App"
 
 export default function IndexPage() {
-  const [city, setCity] = useState<string | null>(null)
-  return (
-    <div className="py-2">
-      <form
-        className="flex items-center justify-center"
-        onSubmit={(e) => {
-          e.preventDefault()
-          const formdata = new FormData(e.currentTarget)
-          setCity(String(formdata.get("city")))
-        }}
-      >
-        <span>Weather Search:</span>{" "}
-        <input
-          data-testid="weather-input"
-          className="px-2 py-1 ml-2 border border-black"
-          type="text"
-          name="city"
-        />
-        <button className="p-2 ml-2 text-sm border rounded-lg" type="submit">
-          Submit
-        </button>
-      </form>
-
-      {city && (
-        <div className="mt-4">
-          <CityWeather city={city} />
-        </div>
-      )}
-    </div>
-  )
+  return <App />
 }
