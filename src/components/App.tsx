35ea099757b0320258cf9162ab846b726299ebfd
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-03 14:43:55 -0700

    fix: add "flex-wrap" and "pb-2 sm:pb-0 text-base sm:text-2xl" to wrapper and Weather Search in <App>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,28 +17,31 @@ export default function App() {
     <>
       <div className="relative z-10 pt-8">
         <form
-          className="flex items-center justify-center"
+          className="flex flex-wrap items-center justify-center"
           onSubmit={(e) => {
             e.preventDefault()
             const formdata = new FormData(e.currentTarget)
             setCity(String(formdata.get("city")))
           }}
         >
-          <h1 className="text-base font-semibold tracking-tight">
+          <h1 className="pb-2 text-2xl font-semibold tracking-tight sm:text-base sm:pb-0">
             Weather Search:
           </h1>
-          <input
-            data-testid="weather-input"
-            className="w-40 h-10 p-2 ml-2 border border-gray-300 border-solid rounded-l-lg"
-            type="text"
-            name="city"
-          />
-          <button
-            className="h-10 p-2 text-xs font-bold text-white uppercase bg-[#4683c8] border border-[#4683c8] border-solid rounded-r-lg"
-            type="submit"
-          >
-            Submit
-          </button>
+          <div>
+            {/* wrapper div to avoid flex-wrap on mobile */}
+            <input
+              data-testid="weather-input"
+              className="w-40 h-10 p-2 ml-2 border border-gray-300 border-solid rounded-l-lg"
+              type="text"
+              name="city"
+            />
+            <button
+              className="h-10 p-2 text-xs font-bold text-white uppercase bg-[#4683c8] border border-[#4683c8] border-solid rounded-r-lg"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
         </form>
 
         {city && <CityWeather city={city} />}
