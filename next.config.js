88385a334caa442eb1439c660eead67eb03a8ba5
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-03 13:28:51 -0700

    fix: remove file-loader loader from next.config.js in favor of Next.js default

--- next.config.js
+++ next.config.js
@@ -9,19 +9,6 @@ module.exports = {
 
   webpack: (config, { dev, isServer, webpack }) => {
     config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next",
-            name: "static/media/[name].[hash].[ext]",
-          },
-        },
-      ],
-    })
-
-    config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     })
