6ae95f62a56ace7d68c482a9ee906341a259ae2f
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-03 14:02:44 -0700

    fix: use <Card> for <Loading> in <CityWeather> and make children optional in <Card>

--- src/components/CityWeather.tsx
+++ src/components/CityWeather.tsx
@@ -25,7 +25,7 @@ export default function CityWeather({ city }: { city?: string }) {
   if (!city) return null
 
   // Display a loading message if we have a city but no weatherResult
-  const Loading = () => <div>loading...</div>
+  const Loading = () => <Card heading="...loading" />
   if (!weatherResult) return <Loading />
 
   // Display an error message if we don't get a 200 HTTP OK response
