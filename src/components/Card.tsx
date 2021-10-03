e93b4ab7eba60f38a57372219270597312131a25
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-03 14:05:49 -0700

    fix: add useSmallFont handler to <Card> to choose "text-2xl" (default) vs "text-base" and match / improve on design document

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+const classNames = (...args: string[]) => args.filter(Boolean).join(" ")
+
 export default function Card({
   children,
   heading,
@@ -5,9 +7,17 @@ export default function Card({
   children?: React.ReactNode
   heading: string
 }) {
+  const useSmallFont = heading.length >= 10 // "MEMPHIS" vs "PUERTO MORELOS"
   return (
     <div className="flex flex-col items-center justify-center w-48 p-4 mx-auto text-gray-400 bg-white rounded-lg shadow-md drop-shadow-md">
-      <h2 className="font-bold text-gray-600 uppercase">{heading}</h2>
+      <h2
+        className={classNames(
+          useSmallFont ? "text-base" : "text-2xl",
+          "font-bold text-gray-600 uppercase"
+        )}
+      >
+        {heading}
+      </h2>
       {children && children}
     </div>
   )
