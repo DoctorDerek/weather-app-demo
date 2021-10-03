01e19c056219d4776ced572afa1e3eedd29be068
Dr. Derek Austin <derekraustin@gmail.com>
2021-10-03 14:10:21 -0700

    fix: reduce "text-2xl" to "text-xl" in <Card> to match design document

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ export default function Card({
     <div className="flex flex-col items-center justify-center w-48 p-4 mx-auto mt-12 text-gray-400 bg-white rounded-lg shadow-md drop-shadow-md">
       <h2
         className={classNames(
-          useSmallFont ? "text-base" : "text-2xl",
+          useSmallFont ? "text-base" : "text-xl",
           "font-bold text-gray-600 uppercase"
         )}
       >
